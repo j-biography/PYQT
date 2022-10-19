b2d461767dd65ca80fccdf58e0b610dc7ab1714a
j-biography <juhjj147@naver.com>
2022-10-19 21:50:43 +0900

    update

--- PYQT_10.py
+++ PYQT_10.py
@@ -13,20 +13,29 @@ class MyApp(QWidget):
     def initUI(self):
         okButton = QPushButton('OK')
         cancelButton = QPushButton('Cancel')
+        # 2개의 버튼 생성
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(okButton)
         hbox.addWidget(cancelButton)
-        hbox.addStretch(1)
+        hbox.addStretch(1) # 수평박스(QHBoxLayout)를 생성하고, 2개의 버튼과 빈 공간(addStretch)을 추가한다.
 
         vbox = QVBoxLayout()
         vbox.addStretch(3)
         vbox.addLayout(hbox)
-        vbox.addStretch(1)
+        vbox.addStretch(1) # 수직박스(QVBoxLayout)를 생성하고, 수평박스(QHBoxLayout = hbox)를 넣어준다.
 
         self.setLayout(vbox)
 
         self.setWindowTitle('Box Layout')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
         
         
